5709fc60f073d3a51f179f6b6613bdc25b0211a1
Ravindu Viduranga Bandara <ravinduviduranga5941@gmail.com>
2025-12-24 21:27:04 +0530

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,1 +1,21 @@
+function doPost(e) {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var data = JSON.parse(e.postData.contents);
+
+  sheet.appendRow([
+    data.invoiceNo,
+    data.date,
+    data.customerName,
+    data.address,
+    data.phone,
+    data.item,
+    data.qty,
+    data.unitPrice,
+    data.total
+  ]);
+
+  return ContentService
+    .createTextOutput(JSON.stringify({status: "success"}))
+    .setMimeType(ContentService.MimeType.JSON);
+}
 
